bff8d5b6ee1cf3de6c503042e00ae839ed57ea61
hansol <onesorry49@gmail.com>
2019-07-21 12:00:00 +0900

    [#1] lol api 연동
    개발 api key로 응답 받아오는거 확인

--- routes/api/match.js
+++ routes/api/match.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const axios = require('axios')
+require('dotenv').config();
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.json({
-    "message": "success"
+router.get('/:matchId', (req, res, next) => {
+  const matchId = req.params.matchId
+  axios.get(`https://kr.api.riotgames.com/lol/match/v4/matches/${matchId}?api_key=${process.env.LOL_API_KEY}`)
+  .then(response => {
+    res.json(response.data)
   })
 })
 
