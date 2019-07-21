221f2d64efecb2c3ad94fd834e9ad4d233be0f93
hansol <onesorry49@gmail.com>
2019-07-21 12:55:59 +0900

    [#2] match-info 만들기
    /match-info/:matchId로 요청 보내면
    해당 게임을 플레이한 10명의 이름, 챔피언 등의 기본 정보를
    JSON으로 만들어 return

--- routes/api/match.js
+++ routes/api/match.js
@@ -18,4 +18,48 @@ router.get('/:matchId', (req, res, next) => {
   })
 })
 
+// matchId로 해당 게임의 플레이어 10명 이름, 챔피언 등의 기본 정보 얻기
+router.get('/match-info/:matchId', (req, res, next) => {
+  const matchId = req.params.matchId
+  const matchUrl = `/lol/match/v4/matches/${matchId}`
+  const timelineUrl = `/lol/match/v4/timelines/by-match/${matchId}`
+
+  const matchPromise = axios.get(baseUrl+matchUrl, {
+    params: {
+      api_key: process.env.LOL_API_KEY
+    }
+  })
+
+  const timelinePromise = axios.get(baseUrl+timelineUrl, {
+    params: {
+      api_key: process.env.LOL_API_KEY
+    }
+  })
+
+  Promise.all([matchPromise, timelinePromise])
+  .then(result => {
+    const matchResponse = result[0].data
+    const timelineResponse = result[1].data
+
+    const playerList = matchResponse.participantIdentities.map(participant => ({ summonerName: participant.player.summonerName, participantId: participant.participantId }))
+
+    const playerInfo = playerList.map(player => {
+      return {
+        ...player,
+        championId: matchResponse.participants[player.participantId - 1].championId,
+        position: {
+          ...timelineResponse.frames[0].participantFrames[player.participantId].position
+        }
+      }
+    })
+
+    const match_info = {
+      "playerInfo" : playerInfo,
+      "gameDuration": matchResponse.gameDuration
+    }
+
+    res.json(match_info)
+  })
+})
+
 module.exports = router;
