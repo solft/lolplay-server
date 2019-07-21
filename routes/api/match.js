c0122d30900307b7c823327bb372614385cf001b
hansol <onesorry49@gmail.com>
2019-07-21 12:17:05 +0900

    api 요청 부분 정리
    api_key를 params로 보내고 baseUrl과 subUrl 분리

--- routes/api/match.js
+++ routes/api/match.js
@@ -3,9 +3,16 @@ const axios = require('axios')
 require('dotenv').config();
 const router = express.Router();
 
+const baseUrl = 'https://kr.api.riotgames.com'
+
 router.get('/:matchId', (req, res, next) => {
   const matchId = req.params.matchId
-  axios.get(`https://kr.api.riotgames.com/lol/match/v4/matches/${matchId}?api_key=${process.env.LOL_API_KEY}`)
+  const subUrl = `/lol/match/v4/matches/${matchId}`
+  axios.get(baseUrl+subUrl, {
+    params: {
+      api_key: process.env.LOL_API_KEY
+    }
+  })
   .then(response => {
     res.json(response.data)
   })
