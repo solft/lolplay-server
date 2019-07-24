11845dc1dbd1846a19b58c4b0d8ecf16ab19811f
hansol <onesorry49@gmail.com>
2019-07-24 23:26:42 +0900

    make input text

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Input = styled.input`
+  display: block;
+  margin: 0;
+  border: none;
+  width: 100%;
+  border-radius: 0.4rem;
+
+  &:focus {
+    outline: none;
+    box-shadow: 0.2rem 0.8rem 1.6rem #4527a0
+  }
+`
 
 const Home = () => {
   return (
     <div>
-      <div>닉네임 검색하는 곳</div>
-      <div>결과로 매치 리스트 출력</div>
-      <div>매치 리스트 검색하면 해당 매치 리플레이 화면으로</div>
+      <Input />
     </div>
   )
 }
